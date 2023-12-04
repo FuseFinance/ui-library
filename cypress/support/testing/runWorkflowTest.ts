32c46f312485d2493a3a1652adfb16cf5bf9c204
Kevin Uzcategui <kevin24u04@gmail.com>
2023-12-04 16:42:02 -0500

    ENG-2277 :: Config StoryBook

--- cypress/support/testing/runWorkflowTest.ts
+++
@@ -1,9 +0,0 @@
-import { TestPageSelector } from '@/cypress/utils/selectors';
-
-Cypress.Commands.add('runWorkflowTest', () => {
-  try {
-    cy.get(TestPageSelector.runTestButton).click();
-  } catch (error) {
-    cy.log(error);
-  }
-});
