6e810c253cdc6cfafcd2e71b0c2f64d9e05d58dd
jaristizabal-fuse <jaristizabal@fusefinance.com>
2024-01-04 09:40:11 -0500

    PENG-175

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+export default {
+  moduleDirectories: ['node_modules', 'src'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^@types/(.*)$': '<rootDir>/src/types/$1',
+  },
+  setupFilesAfterEnv: ['./jest.setup.js'],
+};
