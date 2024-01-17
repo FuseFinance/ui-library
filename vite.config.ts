79a39ca0ce91b3746e2b71f62b9b2704698cb545
Kevin Uzcategui <kevin24u04@gmail.com>
2024-01-17 10:03:51 -0500

    PENG-532 :: Fix NPM library bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
 // <reference types="vitest" />
 
 import path from 'path';
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import { Plugin, defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import checker from 'vite-plugin-checker';
+
+function importCss(): Plugin {
+  return {
+    name: 'import-css', // name of the plugin
+    generateBundle(options, bundle) {
+      // Itera sobre todos los archivos del paquete
+      for (const [fileName, chunkOrAsset] of Object.entries(bundle)) {
+        // Verifica si el archivo es uno de los que quieres modificar
+        if (fileName === 'index.cjs.js' || fileName === 'index.es.js') {
+          // Check if it is a chunk (code file)
+          if (chunkOrAsset.type === 'chunk') {
+            // Add your extra code at the beginning or at the end
+            chunkOrAsset.code += `import "./style.css";`;
+          }
+        }
+      }
+    }
+  };
+}
 
 export default defineConfig({
   resolve: {
@@ -33,16 +52,17 @@ export default defineConfig({
     }),
     viteTsconfigPaths(),
     svgrPlugin(),
+    importCss()
   ],
-  server: {
-    host: true,
-    port: 3000,
-  },
-  preview: {
-    port: 3000,
-  },
   build: {
-    target: 'es6',
+    lib: {
+      entry: "./src/index.ts", // Specifies the entry point for building the library.
+      name: "vite-react-ts-button", // Sets the name of the generated library.
+      fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
+      formats: ["cjs", "es"], // Specifies the output formats (CommonJS and ES modules).
+    },
+    sourcemap: true, // Generates source maps for debugging.
+    emptyOutDir: true, // Clears the output directory before building.
   },
   optimizeDeps: {
     exclude: ['@codemirror/state'],
