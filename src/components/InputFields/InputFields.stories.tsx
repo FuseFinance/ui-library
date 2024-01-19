f811dd3a60237b48167059cd3a0d140c43225894
Kevin Uzcategui <kevin24u04@gmail.com>
2024-01-19 13:18:38 -0500

    PENG-517 :: Create component 'Simple Input' in StoryBook

---
+++ src/components/InputFields/InputFields.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { message } from 'antd';
+import InputFields from './InputFields';
+
+const meta: Meta<typeof InputFields> = {
+  component: InputFields,
+  title: 'From/InputFields',
+  // tags:['autodocs'],
+  argTypes: {
+    disabled: {
+      control: 'boolean',
+    },
+    size: {
+      control: 'select',
+      options: ['small', 'default', 'large'],
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const handleChange = (e) => {
+  message.info(`${e.target.value}`);
+};
+export const Base: Story = {
+  args: {
+    handleOnChangeInput: handleChange,
+  },
+};
