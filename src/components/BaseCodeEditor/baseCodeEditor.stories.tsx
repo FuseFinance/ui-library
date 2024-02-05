eab12e05e5c2af7896bb6247b9d18bcc2c202a43
Kevin Uzcategui <kevin24u04@gmail.com>
2024-02-05 14:17:01 -0500

    PENG-526 :: Create component 'input code with text' (#28)

--- src/components/BaseCodeEditor/baseCodeEditor.stories.tsx
+++ src/components/BaseCodeEditor/baseCodeEditor.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta } from '@storybook/react';
 import BaseCodeEditor from './baseCodeEditor';
 import { action } from '@storybook/addon-actions';
+import { javascript } from '@codemirror/lang-javascript';
 
 export default {
   title: 'Form/CodeEditor',
@@ -8,17 +9,6 @@ export default {
   args:{
     defaultValue: undefined,
     placeholder:'my placeholder',
-    plugins: [
-      {
-        "name": "customVars",
-        "isModule": true,
-        "decorationClass": "code-tag code-editor-hl-formula",
-        "options": {
-            "myOpntionOne()": "customVars"
-        },
-        "matchRegex": /customVars\.(myOpntionOne())\b\((.*?)\)/g
-      }
-    ],
     readonly: false,
     customCSSClass : undefined,
     containerCSSClass : undefined,
@@ -32,6 +22,11 @@ export default {
     customCSSClass: { control: 'text' },
     containerCSSClass: { control: 'text' },
     customContainerAttr: { control: 'object' },
+    extensions: {
+      table: {
+        disable: true,
+      },
+    },    
   },
 } as Meta;
 
@@ -43,10 +38,40 @@ const handleChange = (e) => {
   action('change input')(e);
 };
 
+
 export const CodeEditorInLine = Template.bind({});
 CodeEditorInLine.args = {
+  defaultValue: 'https://myweb.com/api/{{varName}}',
+  onChange: handleChange,
+  placeholder:'my placeholder', 
+  maxLines: 1,
+  plugins: [
+    {
+      "name": "interpolations",
+      "isModule": true,
+      "decorationClass": "code-tag code-editor-hl-interpolations",
+      "options": {},
+      "matchRegex": /\{\{\s*([^}}]*?)\s*\}\}/g
+    }
+  ],    
+};
+
+export const CodeEditorInLineWithJavascript = Template.bind({});
+CodeEditorInLineWithJavascript.args = {
   defaultValue: 'var myVar = customVars.myOpntionOne();',
   onChange: handleChange,
   placeholder:'my placeholder', 
-  maxLines: 1 
+  maxLines: 1,
+  extensions: [javascript()],
+  plugins: [
+    {
+      "name": "customVars",
+      "isModule": true,
+      "decorationClass": "code-tag code-editor-hl-formula",
+      "options": {
+          "myOpntionOne()": "customVars"
+      },
+      "matchRegex": /customVars\.(myOpntionOne())\b\((.*?)\)/g
+    }
+  ],
 };
